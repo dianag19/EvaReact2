1ae9aae6230eb852a29b5f9dfba6d5b1903c4812
Diana <diana.gomez.caicedo@correounivalle.edu.co>
2019-08-30 11:36:49 -0500

    Secuencia en preguntas y progreso

--- screens/question.js
+++ screens/question.js
@@ -5,102 +5,131 @@ import Carousel from 'react-native-snap-carousel';
 import * as Expo from "expo";
 
 export default class Question extends React.Component {
- 
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
+      index: 0,
+      puntosPorPregunta: 100,
+      respuestas: [],
+      questions: ['¿Qué sentiste cuando usaste la aplicación', 'Question 2'],
       modalVisible: false,
-        activeIndex:0,
-        carouselItems: [
+      activeIndex: 0,
+      carouselItems: [
         {
-            title:"Feliz"
+          title: "Feliz"
         },
         {
-            title:"Triste"
+          title: "Triste"
         },
         {
-            title:"Satisfecho"
+          title: "Satisfecho"
         },
         {
-            title:"Insatisfecho"
+          title: "Insatisfecho"
         },
         {
-            title:"Decepcionado"
+          title: "Decepcionado"
         },
         {
-            title:"Emocionado"
+          title: "Emocionado"
         },
         {
-            title:"Aburrido"
+          title: "Aburrido"
         },
         {
-            title:"Enojado"
+          title: "Enojado"
         },
         {
-            title:"Interesado"
+          title: "Interesado"
         },
         {
-            title:"Confundido"
+          title: "Confundido"
         },
         {
-            title:"Impaciente"
+          title: "Impaciente"
         },
         {
-            title:"Inseguro"
+          title: "Inseguro"
         },
         {
-            title:"Perdido"
+          title: "Perdido"
         }
-    ]}    
-  };  
+      ]
+    }
+  };
 
-  _renderItem({item,index}){
+  _renderItem({ item, index }) {
     return (
-        <View style={{justifyContent:'center',alignItems:'center'}}> 
-          <Image style={styles.image} source={require('../assets/images/Huevo-Feliz.png')} />
-          <Text size={20} bold={true}>{item.title}</Text>          
-        </View>
+      <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <Image style={styles.image} source={require('../assets/images/Huevo-Feliz.png')} />
+        <Text size={20} bold={true}>{item.title}</Text>
+      </View>
     )
   }
 
   setModalVisible(visible) {
-    this.setState({modalVisible: visible});
+    this.setState({ modalVisible: visible });
   }
+  submit() {
+    console.log("entra a submit question")
+    let collection = {}
 
+    collection.nombre = this.state.nombre,
+        collection.descripcion = this.state.descripcion
+    console.log(collection)
+
+    var url = 'https://evadjango.herokuapp.com/aplications/';
+
+    fetch(url, {
+        method: 'POST', // or 'PUT'
+        body: JSON.stringify(collection), // data can be `string` or {object}!
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json())
+        .then(response => console.log('Success:', JSON.stringify(response),
+        alert("Aplicación creada correctamente")))
+        .catch(error => console.error('Error:', error));
+}
   render() {
 
     const { navigation, style } = this.props;
-    
+
     return (
       <View  >
-        
+
         <StatusBar barStyle="light-content" />
-        <View  space="between" style={styles.padded}>
-          <View  space="around" style={{ position: 'relative', zIndex: 2 }}
+        <View space="between" style={styles.padded}>
+          <View space="around" style={{ position: 'relative', zIndex: 2 }}
           >
-              <View center>
-                <Button
-                  title="SIGUIENTE"
-                  onPress={() => this.props.navigation.navigate('Progress')}>
-                  SIGUIENTE
+            <View center>
+              <Button
+                title="SIGUIENTE"
+                onPress={() => (this.setState({
+                  index: this.state.index + 1,
+                  puntosPorPregunta: this.state.puntosPorPregunta + 100,
+                }), console.log("index: " + this.state.index), 
+                this.props.navigation.navigate('Progress', { puntos: this.state.puntosPorPregunta }))}>
+                SIGUIENTE
                 </Button>
-              </View>  
+            </View>
             <View center>
               <View center>
                 <Image style={styles.logo} source={require('../assets/images/evalogo.png')} />
               </View>
               <Text bold style={styles.text} >
-                ¿Qué sentiste cuando usaste la aplicación {navigation.state.params.nombreApp}?
+                {this.state.questions[this.state.index]} {navigation.state.params.nombreApp}?
               </Text>
             </View>
-            
 
-            <SafeAreaView style={styles.container}>           
+
+            <SafeAreaView style={styles.container}>
               <TouchableHighlight
                 onPress={
-                    () => { this.carousel._snapToItem(this.state.activeIndex-1)}
+                  () => { this.carousel._snapToItem(this.state.activeIndex - 1) }
                 }>
-                <Image source={require('../assets/images/leftarrow.png')}/>
+                <Image source={require('../assets/images/leftarrow.png')} />
               </TouchableHighlight>
 
               <View>
@@ -110,33 +139,33 @@ export default class Question extends React.Component {
                   sliderWidth={250}
                   itemWidth={250}
                   renderItem={this._renderItem}
-                  onSnapToItem = { index => this.setState({activeIndex:index}) }
+                  onSnapToItem={index => this.setState({ activeIndex: index })}
                 />
               </View>
-              
-              <TouchableHighlight            
-                  onPress={
-                      () => { this.carousel._snapToItem(this.state.activeIndex+1)}
-                  }>
-                  <Image source={require('../assets/images/rightarrow.png')}/>                
-              </TouchableHighlight>         
+
+              <TouchableHighlight
+                onPress={
+                  () => { this.carousel._snapToItem(this.state.activeIndex + 1) }
+                }>
+                <Image source={require('../assets/images/rightarrow.png')} />
+              </TouchableHighlight>
             </SafeAreaView>
             <View>
-              <View center>                
+              <View center>
                 <CheckBox
                   value={this.state.checked}
                   onValueChange={() => this.setState({ checked: !this.state.checked })}
                 />
-              </View> 
+              </View>
             </View>
 
             <View style={{
               justifyContent: 'space-around',
-              }}>
+            }}>
               <View center>
                 <Button
                   title="JUSTIFICAR RESPUESTA"
-                  onPress={() => {this.setModalVisible(true); }}
+                  onPress={() => { this.setModalVisible(true); }}
                 >
                   JUSTIFICAR
                 </Button>
@@ -147,10 +176,10 @@ export default class Question extends React.Component {
                   onPress={() => this.props.navigation.navigate('Progress')}>
                   SIGUIENTE
                 </Button>
-              </View>            
+              </View>
             </View>
 
-             <View style={{marginTop: 22}}>
+            <View style={{ marginTop: 22 }}>
               <Modal
                 animationType="slide"
                 transparent={false}
@@ -158,23 +187,23 @@ export default class Question extends React.Component {
                 onRequestClose={() => {
                   Alert.alert('Modal has been closed.');
                 }}>
-                <View style={{marginTop: 22}}>
+                <View style={{ marginTop: 22 }}>
                   <View>
-                  <View>
-                    <Text bold style={styles.text} >
-                      ¿Por qué y cuándo sentiste esa(s) emoción(es)?
+                    <View>
+                      <Text bold style={styles.text} >
+                        ¿Por qué y cuándo sentiste esa(s) emoción(es)?
                     </Text>
-                  </View>
-                  <View center>
-                    <View style={{padding: 10}}>
-                      <TextInput
-                        style={{height: 80, padding: 10, fontSize: 20}}
-                        placeholder="Justifica tu respuesta"
-                        onChangeText={(text) => this.setState({text})}
-                        value={this.state.text}
-                      />
                     </View>
-                  </View>
+                    <View center>
+                      <View style={{ padding: 10 }}>
+                        <TextInput
+                          style={{ height: 80, padding: 10, fontSize: 20 }}
+                          placeholder="Justifica tu respuesta"
+                          onChangeText={(text) => this.setState({ text })}
+                          value={this.state.text}
+                        />
+                      </View>
+                    </View>
                     <View center>
                       <TouchableHighlight
                         onPress={() => {
@@ -204,7 +233,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  
+
   image: {
     justifyContent: 'center',
     height: 188,
